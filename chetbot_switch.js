dd64965d1529f75db76323391a8edf448f1c84ae
dlawhdgus <212whdgus@gmil.com>
2022-09-08 10:19:10 +0900

    init commit

---
+++ chetbot_switch.js
@@ -0,0 +1,60 @@
+const readline = require("readline");
+ 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+console.log(`
+안녕하세요 종현 챗봇 입니다.
+도움말을 보고싶다면 0,
+종료를 하려면 -1 을 입력해 주세요
+`)
+ 
+
+rl.on("line", (line) => {
+    let command = line;
+
+    switch (command) {
+        case '0' : 
+            console.log('1 ~ 10 까지 입력을 하면 10개의 나라의 인삿말이 나옵니다.');
+            
+        case '1' : 
+            console.log('안녕하세요');
+            break;
+        case '2' : 
+            console.log('hello');
+            break;
+        case '3' : 
+            console.log('Nǐ hǎo');
+            break;
+        case '4' : 
+            console.log("'ahlan");
+            break;
+        case '5' : 
+            console.log('Hola');
+            break;
+        case '6' : 
+            console.log('bonjour');
+            break;
+        case '7' : 
+            console.log('привет');
+            break;
+        case '8' : 
+            console.log('こんにちは');
+            break;
+        case '9' : 
+            console.log('olá');
+            break;
+        case '10' : 
+            console.log('Kamusta');
+            break;
+        case '-1' : 
+            rl.close();
+            break;
+        default : 
+            console.log('숫자를 잘 못 일벽하셨습니다.');
+            break;
+    }
+});
+ 
